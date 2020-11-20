a7a17e1f9c00c6c8100c16cc44d7dfc0c910e0e0
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-11-20 16:32:07 -0300

    initial setup with mysql

---
+++ controllers/atendimentos.js
@@ -0,0 +1,9 @@
+const atendimento = require('../models/atendimentos');
+module.exports = (app) => {
+  app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos GET'));
+  app.post('/atendimentos', (req, res) => {
+    const corpo = req.body;
+    atendimento.adiciona(corpo);
+    res.send(` Você está na rota de atendimentos POST`)
+  });
+}
