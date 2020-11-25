7194e93315bafcc4591bc39de169eb38e4e84ce3
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-11-25 11:48:48 -0300

    Get by id, patch and delete routes

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,22 @@
 const atendimento = require('../models/atendimentos');
 module.exports = (app) => {
-  app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos GET'));
+  app.get('/atendimentos', (req, res) => { atendimento.lista(res); });
+  app.get('/atendimentos/:id', (req, res) => { 
+    const id = parseInt(req.params.id);
+    atendimento.buscaPorId(id, res);
+  });
   app.post('/atendimentos', (req, res) => {
     const corpo = req.body;
     atendimento.adiciona(corpo, res);
   });
+  app.patch('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const valores = req.body;
+
+    atendimento.altera(id, valores, res);
+  }); 
+  app.delete('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    atendimento.deleta(id, res);
+  }); 
 }
