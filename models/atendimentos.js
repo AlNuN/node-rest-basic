d4bb511eae67ab2a9731f8070edc63fc3cf302c2
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-12-04 14:11:42 -0300

    Change client to CPF and consume fake API to find client data based on CPF

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,3 +1,4 @@
+const { default: Axios } = require('axios');
 const moment = require('moment');
 const conexao = require('../infra/conexao');
 
@@ -59,11 +60,14 @@ class Atendimento {
   buscaPorId(id, res) {
     const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
 
-    conexao.query(sql, (erro, resultados) => {
+    conexao.query(sql, async (erro, resultados) => {
       if (erro) {
         res.status(400).json(erro);
       } else {
         const atendimento = resultados[0];
+        const cpf = atendimento.cliente;
+        const { data } = await Axios.get(`http://localhost:8082/${cpf}`);
+        atendimento.cliente = data;
         res.status(200).json(atendimento);
       }
     });
